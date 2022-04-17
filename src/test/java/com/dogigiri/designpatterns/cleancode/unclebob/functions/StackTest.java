aaa7cd426ef0077d2702629545e05570d4facfc0
m-moshtagh <m-moshtagh@pm.me>
2022-04-17 12:45:33 +0430

    Stack using TDD with uncle BOB

--- src/test/java/com/dogigiri/designpatterns/cleancode/unclebob/functions/StackTest.java
+++ src/test/java/com/dogigiri/designpatterns/cleancode/unclebob/functions/StackTest.java
@@ -1,12 +1,72 @@
 package com.dogigiri.designpatterns.cleancode.unclebob.functions;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackTest {
+    Stack stack = BoundedStack.make(3);
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    public void newlyInitializedStackShouldBeEmpty() {
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void stackSizeShouldBeOneAfterFirstPush() {
+        stack.push(1);
+        assertFalse(stack.isEmpty());
+        assertEquals(1, stack.getSize());
+    }
+
+    @Test
+    public void stackShouldBeEmptyAfterOnePushAndPop() {
+        stack.push(1);
+        stack.pop();
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void stackOverflowTest() {
+        assertThrows(BoundedStack.Overflow.class, () -> {
+            stack.push(1);
+            stack.push(1);
+            stack.push(1);
+        });
+    }
+
+    @Test
+    public void stackUnderflowTest() {
+        assertThrows(BoundedStack.Underflow.class, () -> stack.pop());
+    }
+
+    @Test
+    public void illegalCapacityTest() {
+        assertThrows(BoundedStack.IllegalCapacity.class, () -> BoundedStack.make(-1));
+    }
+
+    @Test
+    public void newlyPushedElementShouldBePopped() {
+        stack.push(2);
+        assertEquals(2, stack.pop());
+    }
+
+    @Test
+    void createStackWithZeroCapacity() {
+        Stack zeroStack = BoundedStack.make(0);
+        assertEquals(-1, zeroStack.top());
+    }
+
+    @Test
+    void stackEmptyTopTest() {
+        assertThrows(Stack.Empty.class, () -> stack.top());
+    }
+
+    @Test
+    void findElementsIndexTest() {
+        stack.push(1);
+        stack.push(3);
+        stack.push(4);
+        assertEquals(1, stack.find(3));
     }
 }
